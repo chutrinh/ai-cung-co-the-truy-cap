a7202cddb16327b573ab0242ca23ca408cf31ff2
chu trinh <chutrinh@gmail.com>
2023-11-24 17:47:16 +0700

    edit cors

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "*", // Hoặc dùng "*" nếu bạn muốn cho phép mọi origin
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*", // Hoặc dùng "*" nếu bạn muốn cho phép mọi origin
+//     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/", (req, res, next) => {
   res.json("chú trình");
